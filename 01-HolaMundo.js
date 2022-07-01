3b81359ece6140328ee2bcfada5425359da14f59
Ignacio Femenia <femeniax@gmail.com>
2022-07-01 06:19:54 -0300

    Console log, Const and Definitions

---
+++ 01-HolaMundo.js
@@ -0,0 +1,10 @@
+console.log('Mi primer hola mundo');
+console.log(2 + 2);
+
+const nombreMadre = "Maria";
+const nombrePadre = "Jose";
+const nombreHijo = "Jesus";
+
+const edadMadre = 60;
+console.log(nombreHijo)
+console.log("La edad de mi madre es : " +edadMadre)
