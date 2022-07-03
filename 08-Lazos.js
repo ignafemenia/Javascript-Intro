fb0698d1264eeb0f1f30b7be0198dc41fd703bca
Ignacio Femenia <femeniax@gmail.com>
2022-07-03 11:16:42 -0300

    Last Commit

--- 08-Lazos.js
+++ 08-Lazos.js
@@ -11,13 +11,26 @@ const estaAcompanado = true;
 const tienePasaje = true;
 const ciudadDestino = 'Lima';
 let posicion = 0;
-
+let destinoDisponible = false;
 
 while (posicion < ciudadesDisponibles.length) {
     if (ciudadesDisponibles[0] == ciudadDestino) {
-    console.log(`Destino Disponible`)
+        console.log(`Destino Disponible`)
+        destinoDisponible = true;
+        break;
 } else {
     console.log(`Destino no disponible`)
     }
     posicion = posicion + 1; // o se puede hacer posicion++;
+}
+
+console.log(`El destino disponible`, destinoDisponible);
+
+
+//lazo for
+for (posicion = 0; posicion < ciudadDestino.length; posicion++){
+    if (ciudadDestino[posicion] == ciudadDestino) {
+        destinoDisponible = true;
+        break;
+    }
 }
